608f31e8afed186f7c75490362fecef65cc6fd88
HellDiverOne <81807872+HellDiverOne@users.noreply.github.com>
2022-04-18 15:03:22 +0300

    Add files via upload

---
+++ create tabke data.sql
@@ -0,0 +1,10 @@
+create table data(
+id int primary key identity (1,1),
+title nvarchar,
+genre nvarchar,
+author nvarchar,
+quantityinstore int,
+quantityinstock int,
+description nvarchar,
+cover image
+);
